5899503f58d93449f9e14a7f89e796f00f621ed8
Kumani K <kumanikidd@gmail.com>
2024-01-29 22:10:36 +0000

    created basic footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,28 @@
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+        <div className="container">
+            <div className="footer__content">
+            <div className="footer__logo">
+                {/* <img src={logo} alt="logo" /> */}
+            </div>
+            {/* <div className="footer__social">
+                <a href="https://www.facebook.com/">
+                <img src={facebook} alt="facebook" />
+                </a>
+                <a href="https://www.instagram.com/">
+                <img src={instagram} alt="instagram" />
+                </a>
+                <a href="https://www.twitter.com/">
+                <img src={twitter} alt="twitter" />
+                </a>
+            </div> */}
+            </div>
+        </div>
+        </footer>
+    );
+    }
+
+export default Footer;

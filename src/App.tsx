e3b706986e23a88ec8efe96b1af457787758f3fc
Kumani K <kumanikidd@gmail.com>
2024-02-06 20:47:50 +0000

    nav bvar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Home from './components/Home/Home'
+import Nav from './components/Nav/Nav'
 
 function App() {
 
   return (
     <>
+      <Nav />
+      <Home />
     </>
   )
 }
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,48 @@
+import './Nav.scss';
+
+const Nav = () => {
+    return (
+        <nav className="navbar">
+            <div className="container">
+                <a className="navbar-brand" href="#">Our Classroom</a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">
+                                Home <span className="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                About
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                Services
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;

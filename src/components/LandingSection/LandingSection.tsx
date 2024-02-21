c6af642a381e72e82afbf541b490a84f05a0cd7b
Kumani K <kumanikidd@gmail.com>
2024-02-21 17:04:09 +0000

    landing section added

---
+++ src/components/LandingSection/LandingSection.tsx
@@ -0,0 +1,12 @@
+import './LandingSection.scss';
+
+
+const LandingSection = () => {
+    return (
+        <div className="landing-section">
+            <img src="https://via.placeholder.com/1920x1080" alt="placeholder" className="landing-section__image" />
+        </div>
+    );
+}
+
+export default LandingSection;

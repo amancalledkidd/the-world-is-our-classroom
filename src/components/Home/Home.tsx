228e4cec735a08be1cf9fe36dbfba13e2457d09f
Kumani K <kumanikidd@gmail.com>
2024-02-21 17:05:25 +0000

    added components to home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,18 @@
 import './Home.scss';
+import Nav from '../Nav/Nav';
+import LandingSection from '../LandingSection/LandingSection';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
-        <div className="home">
-        <h1>Home</h1>
-        </div>
+        <>
+            <div className="home">
+                <Nav />
+                <LandingSection />
+                <Banner content="Welcome to our site!" />
+                <h1>Home</h1>
+            </div>
+        </>
     );
     }
 export default Home;

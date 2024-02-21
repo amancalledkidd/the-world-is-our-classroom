a63491a6a1e90e9bfdec3509674f6b08fa74d85d
Kumani K <kumanikidd@gmail.com>
2024-02-21 16:20:00 +0000

    created Nav component

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,46 +1,21 @@
 import './Nav.scss';
+import NavItem from '../NavItem/NavItem';
 
 const Nav = () => {
     return (
         <nav className="navbar">
-            <div className="container">
-                <a className="navbar-brand" href="#">Our Classroom</a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">
-                                Home <span className="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                About
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                Services
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                Contact
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+            <div className="navbar__logo">
+                <h1>Logo</h1>
             </div>
+            <div className="navbar__spacer"></div>
+            <div className="navbar__spacer"></div>
+            <div className="navbar__items">
+                <NavItem name="Home" path="" />
+                <NavItem name="About" path="" />
+                <NavItem name="Contact" path="" />
+                <NavItem name="Blog" path="" />
+            </div>
+            
         </nav>
     );
 }

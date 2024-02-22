2ab1da1c605240ce2697a83817dcd286363fa253
Kumani K <kumanikidd@gmail.com>
2024-02-22 10:31:26 +0000

    added tile section to home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,15 +2,32 @@ import './Home.scss';
 import Nav from '../Nav/Nav';
 import LandingSection from '../LandingSection/LandingSection';
 import Banner from '../Banner/Banner';
+import AboutSection from '../AboutSection/AboutSection';
+import TileSection from '../TileSection/TileSection';
+import TileItem from '../TileItem/TileItem';
+import classroom from '../../assets/images/classroom.webp';
 
 const Home = () => {
+
     return (
         <>
             <div className="home">
                 <Nav />
                 <LandingSection />
                 <Banner content="Welcome to our site!" />
-                <h1>Home</h1>
+                <AboutSection />
+                <TileSection tiles={[
+                    <TileItem image={classroom} />,
+                    <TileItem title="Tile 2" content="This is the second tile. Random text loool hello how do you do wnahts good,  how does this wrtiing effect the tile, hopefu;lly it looks normal. Best practice I think would be to create a type and data file for tile content" />,
+                    <TileItem title="Tile 3" content="This is the third tile" />,
+                    <TileItem image={classroom} />]}
+                />
+                {/* <TileSection tiles={[
+                    <TileItem title="Tile 5" content="This is the fifth tile" />,
+                    <TileItem title="Tile 6" content="This is the sixth tile" />,
+                    <TileItem title="Tile 7" content="This is the seventh tile" />,
+                    <TileItem title="Tile 8" content="This is the eighth tile" />]}
+                /> */}
             </div>
         </>
     );
